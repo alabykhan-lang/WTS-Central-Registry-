56360ab94f0ddeee717cafe199641fca9a91fafc
alabykhan-lang <alabykhan@gmail.com>
2026-08-13 14:41:00 +0100

    Correct PKCE base64url encoding

--- identity-login.js
+++ identity-login.js
@@ -8,7 +8,7 @@
   function base64Url(bytes) {
     let binary = "";
     for (const byte of bytes) binary += String.fromCharCode(byte);
-    return btoa(binary).replace(/\\+/g, "-").replace(/\\//g, "_").replace(/=+$/g, "");
+    return btoa(binary).replace(/\+/g, "-").replace(/\//g, "_").replace(/=+$/g, "");
   }
 
   function randomToken() {
